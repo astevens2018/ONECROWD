39d3be08ae3deaa5b0ec8c42279b3b8bee0d2bd5
Alex Stevens <astev2018@gmail.com>
2018-11-08 15:17:13 +0100

    start

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_06_143344) do
+ActiveRecord::Schema.define(version: 2018_11_07_102009) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cars", force: :cascade do |t|
+    t.string "name"
+    t.integer "year"
+    t.integer "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
